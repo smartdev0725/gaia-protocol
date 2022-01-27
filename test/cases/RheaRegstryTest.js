5b748b9888a7583545c60b7a3bbae9a3b4c0266a
smartdev0725 <cetbdb23@gmail.com>
2022-01-27 13:55:14 -0800

    add more tests for purchase(), refactor

--- test/cases/RheaRegstryTest.js
+++ test/cases/RheaRegstryTest.js
@@ -27,7 +27,6 @@ const {
 contract('RheaRegistry Test', ([
   governor,
   minter,
-  burner,
   operator,
   batchOwner,
   buyer1,
@@ -35,6 +34,7 @@ contract('RheaRegistry Test', ([
   offsetter1,
   rheaGeTokenMock,
   etherAddress,
+  nonWhitelistedTokenMock,
 ]) => {
   const batchDataBase = {
     serialNumber: '1234567',
@@ -57,7 +57,7 @@ contract('RheaRegistry Test', ([
 
     this.payManager = await PaymentManager.new(this.roleManager.address, etherAddress);
 
-    await this.payManager.addTokensToWhitelist([ this.payToken.address ]);
+    await this.payManager.addTokensToWhitelist([ this.payToken.address, etherAddress ]);
 
     this.registry = await Registry.new(
       this.rheaGe.address,
@@ -137,21 +137,24 @@ contract('RheaRegistry Test', ([
   });
 
   describe('#purchase()', () => {
-    it('should transfer minted tokens to 2 different clients who pay with ERC20', async function () {
+    before(async function () {
       const newBatch = {
         ...batchDataBase,
         serialNumber: '2222222',
+        units: new BigNumber(1000000),
       };
-
-      const rheaGeAmt1 = new BigNumber(50);
-      const rheaGeAmt2 = new BigNumber(35);
-      const payAmt1 = new BigNumber(79);
-      const payAmt2 = new BigNumber(52);
 
       await this.registry.generateBatch(
         ...Object.values(newBatch),
         { from: minter }
       ).should.be.fulfilled;
+    });
+
+    it('should transfer minted tokens to 2 different clients who pay with ERC20', async function () {
+      const rheaGeAmt1 = new BigNumber(50);
+      const rheaGeAmt2 = new BigNumber(35);
+      const payAmt1 = new BigNumber(79);
+      const payAmt2 = new BigNumber(52);
 
       await this.payToken.transfer(buyer2, payAmt2, { from: buyer1 });
 
@@ -190,25 +193,134 @@ contract('RheaRegistry Test', ([
       );
     });
 
-    it('should NOT transfer more than was minted', async function () {
-      const newBatch = {
-        ...batchDataBase,
-        serialNumber: '11111',
-        units: new BigNumber(10),
-      };
-
-      const transferAmt = new BigNumber(1000);
+    it('should NOT transfer more ERC20 than was minted', async function () {
+      const rgAmount = new BigNumber(1000000000);
       const payAmt = new BigNumber(15000);
 
       await this.payToken.approve(this.payManager.address, payAmt, { from: buyer1 });
 
-      await this.registry.generateBatch(
-        ...Object.values(newBatch),
-        { from: minter }
-      ).should.be.fulfilled;
-
-      await this.registry.purchase(buyer1, this.payToken.address, payAmt, transferAmt, { from: operator })
+      await this.registry.purchase(buyer1, this.payToken.address, payAmt, rgAmount, { from: operator })
         .should.be.rejectedWith('ERC20: transfer amount exceeds balance');
+    });
+
+    it('should transfer minted tokens to 2 different clients who pay with ETH', async function () {
+      const rheaGeAmt1 = new BigNumber(50);
+      const rheaGeAmt2 = new BigNumber(35);
+      const payAmt1 = new BigNumber(79);
+      const payAmt2 = new BigNumber(52);
+
+      const client1BalanceBefore = await this.rheaGe.balanceOf(buyer1);
+      const client2BalanceBefore = await this.rheaGe.balanceOf(buyer2);
+      // TODO: figure out who calls this function and if these commented balances should change here !!!
+      // const cl1EthBalBefore = new BigNumber(await web3.eth.getBalance(buyer1));
+      // const cl2EthBalBefore = new BigNumber(await web3.eth.getBalance(buyer2));
+      const registryBalBefore = new BigNumber(await web3.eth.getBalance(this.registry.address));
+
+      await this.registry.purchase(buyer1, etherAddress, payAmt1, rheaGeAmt1, { from: operator, value: payAmt1 });
+      await this.registry.purchase(buyer2, etherAddress, payAmt2, rheaGeAmt2, { from: operator, value: payAmt2 });
+
+      const client1BalanceAfter = await this.rheaGe.balanceOf(buyer1);
+      const client2BalanceAfter = await this.rheaGe.balanceOf(buyer2);
+      // const cl1EthBalAfter = new BigNumber(await web3.eth.getBalance(buyer1));
+      // const cl2EthBalAfter = new BigNumber(await web3.eth.getBalance(buyer2));
+      const registryBalAfter = new BigNumber(await web3.eth.getBalance(this.registry.address));
+
+      client1BalanceAfter.sub(client1BalanceBefore).should.be.bignumber.equal(rheaGeAmt1);
+      client2BalanceAfter.sub(client2BalanceBefore).should.be.bignumber.equal(rheaGeAmt2);
+      // TODO: see TODO above
+      // cl1EthBalAfter.sub(cl1EthBalBefore).should.be.bignumber.equal(payAmt1);
+      // cl2EthBalAfter.sub(cl2EthBalBefore).should.be.bignumber.equal(payAmt2);
+
+      registryBalAfter.sub(registryBalBefore).should.be.bignumber.equal(
+        payAmt1.add(payAmt2)
+      );
+    });
+
+    it('should NOT transfer if amount and msg.value differ when paying with ETH', async function () {
+      const rheaGeAmt = new BigNumber(50);
+      const payAmt = new BigNumber(79);
+      const incorrectPayAmt = payAmt.div(new BigNumber(2));
+
+      await this.registry.purchase(
+        buyer1,
+        etherAddress,
+        incorrectPayAmt,
+        rheaGeAmt,
+        { from: operator, value: payAmt }
+      ).should.be.rejectedWith('PaymentManager::collectPayment: incorrect amount has been passed with ETH purchase');
+    });
+
+    it('should NOT transfer if ETH has been sent with an ERC20 purchase', async function () {
+      const rheaGeAmt = new BigNumber(50);
+      const payAmt = new BigNumber(79);
+
+      await this.registry.purchase(
+        buyer1,
+        this.payToken.address,
+        payAmt,
+        rheaGeAmt,
+        { from: operator, value: payAmt }
+      ).should.be.rejectedWith('PaymentManager::collectPayment: ETH has been sent with an ERC20 purchase');
+    });
+
+    it('should NOT transfer if payment was zero with an ERC20 or ETH purchase', async function () {
+      const rheaGeAmt = new BigNumber(50);
+      const zeroAmt = new BigNumber(0);
+
+      await this.registry.purchase(
+        buyer1,
+        this.payToken.address,
+        zeroAmt,
+        rheaGeAmt,
+        { from: operator }
+      ).should.be.rejectedWith('PaymentManager::collectPayment: no payment provided');
+
+      await this.registry.purchase(
+        buyer1,
+        this.payToken.address,
+        zeroAmt,
+        rheaGeAmt,
+        { from: operator, value: zeroAmt }
+      ).should.be.rejectedWith('PaymentManager::collectPayment: no payment provided');
+    });
+
+    it('should NOT transfer if paying with non-whitelisted token', async function () {
+      const rheaGeAmt = new BigNumber(50);
+      const payAmt = new BigNumber(79);
+
+      await this.registry.purchase(
+        buyer1,
+        nonWhitelistedTokenMock,
+        payAmt,
+        rheaGeAmt,
+        { from: operator }
+      ).should.be.rejectedWith('PaymentManager::validateToken: Token is not whitelisted');
+    });
+
+    it('should NOT transfer if client has insufficient funds in ETH', async function () {
+      const rheaGeAmt = new BigNumber(50);
+      const payAmt = new BigNumber(10000000000000).mul(new BigNumber(10).pow(new BigNumber(18)));
+
+      await this.registry.purchase(
+        buyer1,
+        etherAddress,
+        payAmt,
+        rheaGeAmt,
+        { from: operator, value: payAmt }
+      ).should.be.rejectedWith('sender doesn\'t have enough funds to send tx.');
+    });
+
+    it('should NOT transfer if client has not approved the payment amount with an ERC20 purchase', async function () {
+      const rheaGeAmt = new BigNumber(50);
+      const payAmt = new BigNumber(79);
+
+      await this.registry.purchase(
+        buyer1,
+        this.payToken.address,
+        payAmt,
+        rheaGeAmt,
+        { from: operator }
+      ).should.be.rejectedWith('ERC20: transfer amount exceeds allowance');
     });
   });
 
