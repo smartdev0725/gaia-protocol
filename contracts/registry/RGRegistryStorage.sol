0321661e60efe5e5dbf0e345ac4f2b49b1687bc6
smartdev0725 <cetbdb23@gmail.com>
2022-01-31 18:40:35 -0800

    inherit Resolvable and create umbrella interface

--- contracts/registry/RGRegistryStorage.sol
+++ contracts/registry/RGRegistryStorage.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.11;
 import "../utils/OnlyRouterAccess.sol";
 import "../access/RoleAware.sol";
 import "./IRGRegistryStorage.sol";
+import "../proxy-base/Resolvable.sol";
 
 
-contract RGRegistryStorage is RoleAware, OnlyRouterAccess, IRGRegistryStorage {
+contract RGRegistryStorage is RoleAware, Resolvable, OnlyRouterAccess, IRGRegistryStorage {
     bool public override initialized = false;
 
     address public override rheaGeToken;
