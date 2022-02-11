0d9b3a49a160e2c4804f54ff1fe044a955edaca2
smartdev0725 <cetbdb23@gmail.com>
2022-02-11 22:12:19 +0400

    Fix lint

--- test/cases/RheaGeTokenUpgradeTest.js
+++ test/cases/RheaGeTokenUpgradeTest.js
@@ -165,53 +165,53 @@ contract.only('RheaGeToken Upgrade Tests', ([
       await this.rheaGe.init(this.roleManager.address)
         .should.be.rejectedWith('Initializable: contract is already initialized');
     });
-  
+
     it('should NOT transfer before minting', async function () {
       await this.rheaGe.transfer(client2, new BigNumber(10), { from: client1 })
         .should.be.rejectedWith('ERC20: transfer amount exceeds balance');
     });
-  
+
     it('should mint with MINTER_ROLE', async function () {
       const amount = new BigNumber(1000);
       await this.rheaGe.mint(moneybag, amount, { from: minter }).should.be.fulfilled;
     });
-  
+
     it('should NOT mint without MINTER_ROLE', async function () {
       const amount = new BigNumber(1000);
       await this.rheaGe.mint(moneybag, amount, { from: governor })
         .should.be.rejectedWith('RoleAware: Permission denied to execute this function');
     });
-  
+
     it('should NOT transfer to zero address', async function () {
       const amount = new BigNumber(10);
       await this.rheaGe.transfer(zeroAddress, amount, { from: moneybag })
         .should.be.rejectedWith('ERC20: transfer to the zero address');
     });
-  
+
     it('should transfer a few tokens', async function () {
       const amount = new BigNumber(50);
       await this.rheaGe.transfer(receiver, amount, { from: moneybag })
         .should.be.fulfilled;
     });
-  
+
     it('should transfer zero amount', async function () {
       const amount = new BigNumber(0);
       await this.rheaGe.transfer(clientWithoutTokens, amount, { from: clientWithoutTokens })
         .should.be.fulfilled;
     });
-  
+
     it('should NOT transfer from zero balance', async function () {
       const amount = new BigNumber(10);
       await this.rheaGe.transfer(moneybag, amount, { from: clientWithoutTokens })
         .should.be.rejectedWith('ERC20: transfer amount exceeds balance');
     });
-  
+
     it('should approve if balance is zero', async function () {
       const amount = new BigNumber(10);
       await this.rheaGe.approve(moneybag, amount, { from: clientWithoutTokens })
         .should.be.fulfilled;
     });
-  
+
     it('should change allowance', async function () {
       const allowanceBefore = await this.rheaGe.allowance(moneybag, clientWithoutTokens);
       const increaseToAmount = new BigNumber(200);
@@ -220,33 +220,33 @@ contract.only('RheaGeToken Upgrade Tests', ([
       const allowanceAfter = await this.rheaGe.allowance(moneybag, clientWithoutTokens);
       allowanceBefore.should.be.bignumber.equal(allowanceAfter.sub(increaseToAmount));
     });
-  
+
     it('should set allowance to zero', async function () {
       const initialAmount = new BigNumber(200);
       await this.rheaGe.approve(clientWithoutTokens, initialAmount, { from: moneybag })
         .should.be.fulfilled;
       const allowanceBefore = await this.rheaGe.allowance(moneybag, clientWithoutTokens);
       allowanceBefore.should.be.bignumber.equal(initialAmount);
-  
+
       const zeroAmount = new BigNumber(0);
       await this.rheaGe.approve(clientWithoutTokens, zeroAmount, { from: moneybag })
         .should.be.fulfilled;
       const allowanceAfter = await this.rheaGe.allowance(moneybag, clientWithoutTokens);
       allowanceAfter.should.be.bignumber.equal(zeroAmount);
     });
-  
+
     it('should approve to spend infinity', async function () {
       const infinity = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
       await this.rheaGe.approve(client1, infinity, { from: moneybag })
         .should.be.fulfilled;
     });
-  
+
     it('should NOT spend tokens without approval', async function () {
       const amount = new BigNumber(10);
       await this.rheaGe.transferFrom(moneybag, client1, amount, { from: client2 })
         .should.be.rejectedWith('ERC20: transfer amount exceeds allowance');
     });
-  
+
     it('should spend approved tokens', async function () {
       const amount = new BigNumber(10);
       const recipient = '0x0000000000000000000000000000000000000001';
@@ -255,7 +255,7 @@ contract.only('RheaGeToken Upgrade Tests', ([
       await this.rheaGe.transferFrom(moneybag, recipient, amount, { from: clientWithoutTokens })
         .should.be.fulfilled;
     });
-  
+
     it('should NOT spend from zero balance', async function () {
       const amount = new BigNumber(10);
       await this.rheaGe.approve(client1, amount, { from: clientWithoutTokens })
@@ -263,11 +263,11 @@ contract.only('RheaGeToken Upgrade Tests', ([
       await this.rheaGe.transferFrom(clientWithoutTokens, client2, amount, { from: client1 })
         .should.be.rejectedWith('ERC20: transfer amount exceeds balance');
     });
-  
+
     it('should find and match Transfer (mint) event', async function () {
       const amount = new BigNumber(20);
       await this.rheaGe.mint(moneybag, amount, { from: minter }).should.be.fulfilled;
-  
+
       const transferEvent = (await this.rheaGe.getPastEvents('Transfer')).at(-1).args;
       transferEvent.from.should.be.equal(zeroAddress);
       transferEvent.to.should.be.equal(moneybag);
